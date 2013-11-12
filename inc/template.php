8a502e08281d0d93b770d3db82028d355ec45d77
James Cole <jcole@newsignature.com>
2013-11-12 08:47:02 -0500

    document shorten function; create TODO for excerpt length

--- inc/template.php
+++ inc/template.php
@@ -16,11 +16,20 @@ class Template
 
     $content = (!empty($post->post_excerpt)) ? self::shorten( $post->post_excerpt ) : self::shorten( $post->post_content );
 
+    if ( is_search() ) {
+      $content = self::embolden_replace( $content );
+    }
+
     return $content . ' <a href="' . get_permalink() . '">Read More &raquo;</a>';
   }
 
   /*
    * Smartly truncate text
+   *
+   * @param $input        text to shorten
+   * @param $ellipses     should ellipses be added
+   * @param $strip_html   strip HTML tags
+   * @param $length       number of characters in truncated text
    */
   public static function shorten( $input, $ellipses = true, $strip_html = true, $length = 300 )
   {
@@ -35,6 +44,8 @@ class Template
       return $input;
     }
 
+    // TODO: create excerpt length variable in Theme Options
+
     // find last space within length
     $last_space   = strrpos( substr( $input, 0, $length ), ' ' );
     $trimmed_text = substr( $input, 0, $last_space );
@@ -45,6 +56,29 @@ class Template
     }
 
     return $trimmed_text;
+  }
+
+  /*
+   * Highlight searched terms in search results
+   *
+   * @param $subject text to embolden replace
+   */
+  public static function embolden_replace( $subject )
+  {
+    $search       = get_search_query();
+    $search_terms = explode( " ", $search );
+    $patterns     = array();
+    $replacements = array();
+    foreach ( $search_terms as $key => $value ) {
+      $patterns[ ]     = '/' . strtolower( $value ) . '/';
+      $patterns[ ]     = '/' . strtoupper( $value ) . '/';
+      $patterns[ ]     = '/' . ucwords( $value ) . '/';
+      $replacements[ ] = '<strong class="search-result">' . strtolower( $value ) . '</strong>';
+      $replacements[ ] = '<strong class="search-result">' . strtoupper( $value ) . '</strong>';
+      $replacements[ ] = '<strong class="search-result">' . ucwords( $value ) . '</strong>';
+    }
+    $subject = preg_replace( $patterns, $replacements, strip_tags( $subject ) );
+    return $subject;
   }
 
 }

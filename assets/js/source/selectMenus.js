b2d43fd3fcd27ffdcd62027c0cfa1b54d1b88d3d
James Cole <jcole@newsignature.com>
2013-12-01 12:46:19 -0500

    make pretty select menus

--- assets/js/source/selectMenus.js
+++ assets/js/source/selectMenus.js
@@ -10,7 +10,7 @@
       _this = $(this);
 
       _this.wrap('<div class="select-box"/>');
-      _this.after('<span class="select-text"></span><span class="select-arrow"><i class="icon-caret-down"></i></span>');
+      _this.after('<span class="select-text"></span><span class="select-arrow">&#9660;</span>');
 
       _val = $(this).children("option:selected").text();
       _this.next(".select-text").text(_val);
